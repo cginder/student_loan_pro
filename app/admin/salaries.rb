825af33bff607bff4b4fe481d35ef672cd3444f7
Raghu Betina <raghu@firstdraft.com>
2018-11-22 02:21:16 +0000

    Whitelist columns in Salary for ActiveAdmin

--- app/admin/salaries.rb
+++ app/admin/salaries.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Salary do
+
+ permit_params :monthly_wage, :monthly_benefits, :month_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
