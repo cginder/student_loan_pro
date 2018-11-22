898f749fc12b29f6abe290365fd466340dafe5c1
Raghu Betina <raghu@firstdraft.com>
2018-11-22 02:21:09 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "salaries#index"
   # Routes for the Child resource:
 
