72762e6ae50a93454d74ba4e56db15d941b4cb00
Raghu Betina <raghu@firstdraft.com>
2018-11-22 02:21:19 +0000

    Whitelist columns in Child for ActiveAdmin

--- app/admin/children.rb
+++ app/admin/children.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Child do
+
+ permit_params :children_id, :children_date
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
